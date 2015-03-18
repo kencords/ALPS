829b8ee6b3ee2c73d9a665601ecdf6d280ec2b52
suguspnk <antonio_n25@yahoo.com>
2015-03-18 11:18:44 +0800

    latest

--- src/view/UserProfile_Form.java
+++ src/view/UserProfile_Form.java
@@ -12,7 +12,11 @@ public class UserProfile_Form extends Form {
 	public JLabel lblUsername;
 	private JTextField usernameTF;
 	public JTextField currentPasswordPF;
-	public JLabel lblPassword;
+	public JLabel lblCurrentPassword;
+	public JPasswordField newPasswordField;
+	public JLabel lblNewPassword;
+	public JPasswordField passwordField;
+	public JLabel lblConfirmNewPassword;
 
 	public UserProfile_Form() {
 		
@@ -36,13 +40,33 @@ public class UserProfile_Form extends Form {
 		
 		currentPasswordPF = new JPasswordField();
 		currentPasswordPF.setFont(new Font("Comic Sans MS", Font.PLAIN, 18));
-		currentPasswordPF.setBounds(12, 148, 276, 30);
+		currentPasswordPF.setBounds(12, 139, 276, 30);
 		getContentPane().add(currentPasswordPF);
 		
-		lblPassword = new JLabel("Password:");
-		lblPassword.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
-		lblPassword.setBounds(12, 111, 276, 28);
-		getContentPane().add(lblPassword);
+		lblCurrentPassword = new JLabel("Current Password:");
+		lblCurrentPassword.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		lblCurrentPassword.setBounds(12, 102, 276, 28);
+		getContentPane().add(lblCurrentPassword);
+		
+		newPasswordField = new JPasswordField();
+		newPasswordField.setFont(new Font("Comic Sans MS", Font.PLAIN, 18));
+		newPasswordField.setBounds(12, 218, 276, 30);
+		getContentPane().add(newPasswordField);
+		
+		lblNewPassword = new JLabel("New Password:");
+		lblNewPassword.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		lblNewPassword.setBounds(12, 181, 276, 28);
+		getContentPane().add(lblNewPassword);
+		
+		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Comic Sans MS", Font.PLAIN, 18));
+		passwordField.setBounds(12, 297, 276, 30);
+		getContentPane().add(passwordField);
+		
+		lblConfirmNewPassword = new JLabel("Confirm New Password:");
+		lblConfirmNewPassword.setFont(new Font("Comic Sans MS", Font.PLAIN, 20));
+		lblConfirmNewPassword.setBounds(12, 260, 276, 28);
+		getContentPane().add(lblConfirmNewPassword);
 	}
 	
 	@Override

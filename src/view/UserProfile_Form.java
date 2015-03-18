54f06af35717bc86ee139f8d69992a03481821b8
suguspnk <antonio_n25@yahoo.com>
2015-03-18 10:19:27 +0800

    User Profile Form

---
+++ src/view/UserProfile_Form.java
@@ -0,0 +1,14 @@
+package view;
+
+@SuppressWarnings("serial")
+public class UserProfile_Form extends Form {
+
+	public UserProfile_Form() {
+	}
+	
+	@Override
+	public void verifyInput() {
+
+	}
+
+}
